bbe484fe4691a7c1d62fbb24f0071898e3c0c2e2
Emanuel Nicolas Herrador <emanuel.nicolas.herrador@unc.edu.ar>
2023-05-04 13:31:02 -0300

    Terminada HTTP request Realizado por el grupo en conjunto usando LiveShare en VSCode

--- src/httpRequest/httpRequester.java
+++ src/httpRequest/httpRequester.java
@@ -1,5 +1,11 @@
 package httpRequest;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /* Esta clase se encarga de realizar efectivamente el pedido de feed al servidor de noticias
  * Leer sobre como hacer una http request en java
@@ -7,15 +13,36 @@ package httpRequest;
  * */
 
 public class httpRequester {
-	
-	public String getFeedRss(String urlFeed){
-		String feedRssXml = null;
-		return feedRssXml;
+	public static String getFeedRss(String urlFeed) throws MalformedURLException, IOException {
+		StringBuffer feedRssXml = new StringBuffer();
+		
+		// Creamos URL de solicitud y abrimos conexión HTTP
+		URL url = new URL(urlFeed);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setInstanceFollowRedirects(true);
+				
+		// Leer status code y conseguir contenido
+		int status = con.getResponseCode();
+
+		BufferedReader streamReader;
+
+		if (status > 299) {
+			streamReader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		} else {
+			streamReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		}
+		String inputLine;
+
+		while ((inputLine = streamReader.readLine()) != null) {
+			feedRssXml.append(inputLine);
+		}
+		
+		return feedRssXml.toString();
 	}
 
-	public String getFeedReedit(String urlFeed) {
+	public static String getFeedReedit(String urlFeed) {
 		String feedReeditJson = null;
 		return feedReeditJson;
 	}
-
 }
