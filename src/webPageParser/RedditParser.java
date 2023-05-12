41fb4f5380db853089a375b8589735d2a121e46b
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2023-05-12 20:14:32 -0300

    fix reddit parser

--- src/webPageParser/RedditParser.java
+++ src/webPageParser/RedditParser.java
@@ -14,36 +14,37 @@ import org.json.JSONObject;
 
 public class RedditParser {
     public Feed parse(String content) throws EmptyFeedException {
-        return null;
-    //     Feed feed = new Feed("Unnamed Feed");
-    //     StringReader reader = new StringReader(content);
-    //     JSONTokener tokener = new JSONTokener(reader);
-    //     JSONObject listing = new JSONObject(tokener);
-    //     JSONArray articles = listing.getJSONArray("children");
+        // return null;
+        Feed feed = new Feed("Unnamed Feed");
+        StringReader reader = new StringReader(content);
+        JSONTokener tokener = new JSONTokener(reader);
+        JSONObject listing = new JSONObject(tokener);
+        JSONObject data = listing.getJSONObject("data");
+        JSONArray articles = data.getJSONArray("children");
 
-    //     if(articles.length() == 0) {
-    //         throw new EmptyFeedException("Feed vacio");
-    //     } else {
-    //         JSONObject firstArticle = articles.getJSONObject(0);
-    //         feed.setSiteName(firstArticle.getString("subreddit"));
-    //     }
+        if(articles.length() == 0) {
+            throw new EmptyFeedException("Feed vacio");
+        } else {
+            JSONObject firstArticle = articles.getJSONObject(0).getJSONObject("data");
+            feed.setSiteName(firstArticle.getString("subreddit"));
+        }
 
-    //     for (int i = 0; i < articles.length(); i++) {
-    //         JSONObject article = articles.getJSONObject(i);
+        for (int i = 0; i < articles.length(); i++) {
+            JSONObject article = articles.getJSONObject(i).getJSONObject("data");
 
-    //         String title = article.getString("title");
-    //         Number timestamp = article.getNumber("created");
-    //         String text = article.getString("self_text");
-    //         String link = article.getString("url");
+            String title = article.getString("title");
+            Number timestamp = article.getNumber("created");
+            String text = article.getString("selftext");
+            String link = article.getString("url");
 
-    //         Date date = new Date(timestamp.longValue());
+            Date date = new Date(timestamp.longValue());
 
-    //         Article art = new Article(title, text, date, link);
+            Article art = new Article(title, text, date, link);
 
-    //         feed.addArticle(art);
-    //     }
+            feed.addArticle(art);
+        }
 
 
-    //     return feed;
+        return feed;
     }
 }
